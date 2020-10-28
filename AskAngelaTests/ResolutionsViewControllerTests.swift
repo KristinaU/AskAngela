92c9474d7f4ed8b20dba4c0499c6c4c50a55c46a
Kristina Urosova <urosov.k@gmail.com>
2020-10-28 19:02:14 +0000

    Test properties for resolution view

--- AskAngelaTests/ResolutionsViewControllerTests.swift
+++ AskAngelaTests/ResolutionsViewControllerTests.swift
@@ -1,9 +1,33 @@
 //
-//  ResolutionsViewControllerTests.swift
-//  AskAngela
+//  ViewControllerTests.swift
+//  AskAngelaTests
 //
 //  Created by The App Experts on 28/10/2020.
 //  Copyright © 2020 The App Experts. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import AskAngela
+
+class ResolutionsViewControllerTests: XCTestCase {
+    private var sut: ResolutionsViewController!
+
+    override func setUp() {
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: "ResolutionsViewController") as? ResolutionsViewController
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_outlets_shouldBeConnected() {
+        sut.loadViewIfNeeded()
+
+        XCTAssertNotNil(sut.resolutionsTableView)
+        XCTAssertNotNil(sut.resolutionsArray)
+        
+    }
+}
